75b58ae8796ae67f09b4750bd72223269a8b8daf
your name <sawad7007@gmail.com>
2025-01-16 15:19:02 +0530

    new

--- leet.js
+++ leet.js
@@ -1,13 +1,44 @@
 
-var createCounter = function(n) {
-  count=n;
+// var createCounter = function(n) {
+//   count=n;
   
-  return function() {
-      return count++
+//   return function() {
+//       return count++
       
-  };
-};
+//   };
+// };
 
-counter=createCounter(10);
-console.log(counter());
-console.log(counter());
+// counter=createCounter(10);
+// console.log(counter());
+// console.log(counter());
+
+
+function expect(val) {
+  return {
+    toBe: function (expectedVal) {
+      if (val === expectedVal) {
+        console.log("Values are equal!");
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+    notToBe: function (expectedVal) {
+      if (val !== expectedVal) {
+        console.log("Values are not equal!");
+        return true;
+      } else {
+        throw new Error("Equal");
+      }
+    },
+  };
+}
+
+// Example Usage:
+try {
+  expect(5).toBe(5); // Output: Values are equal!
+  expect(5).notToBe(10); // Output: Values are not equal!
+  expect(5).toBe(10); // Throws Error: "Not Equal"
+} catch (error) {
+  console.error(error.message);
+}
