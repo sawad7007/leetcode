1b1abda1e41b51c9d3e411044d1fbe6e87a324b1
your name <sawad7007@gmail.com>
2025-01-16 15:22:08 +0530

    testing

--- leet.js
+++ leet.js
@@ -13,32 +13,58 @@
 // console.log(counter());
 
 
-function expect(val) {
+// function expect(val) {
+//   return {
+//     toBe: function (expectedVal) {
+//       if (val === expectedVal) {
+//         console.log("Values are equal!");
+//         return true;
+//       } else {
+//         throw new Error("Not Equal");
+//       }
+//     },
+//     notToBe: function (expectedVal) {
+//       if (val !== expectedVal) {
+//         console.log("Values are not equal!");
+//         return true;
+//       } else {
+//         throw new Error("Equal");
+//       }
+//     },
+//   };
+// }
+
+// // Example Usage:
+// try {
+//   expect(5).toBe(5); // Output: Values are equal!
+//   expect(5).notToBe(10); // Output: Values are not equal!
+//   expect(5).toBe(10); // Throws Error: "Not Equal"
+// } catch (error) {
+//   console.error(error.message);
+// }
+
+function createCounter(init) {
+  let currentValue = init; // Initialize the current value with the given init value
+
   return {
-    toBe: function (expectedVal) {
-      if (val === expectedVal) {
-        console.log("Values are equal!");
-        return true;
-      } else {
-        throw new Error("Not Equal");
-      }
+    increment: function () {
+      currentValue += 1; // Increase currentValue by 1
+      return currentValue;
     },
-    notToBe: function (expectedVal) {
-      if (val !== expectedVal) {
-        console.log("Values are not equal!");
-        return true;
-      } else {
-        throw new Error("Equal");
-      }
+    decrement: function () {
+      currentValue -= 1; // Decrease currentValue by 1
+      return currentValue;
+    },
+    reset: function () {
+      currentValue = init; // Reset currentValue to the initial value
+      return currentValue;
     },
   };
 }
 
 // Example Usage:
-try {
-  expect(5).toBe(5); // Output: Values are equal!
-  expect(5).notToBe(10); // Output: Values are not equal!
-  expect(5).toBe(10); // Throws Error: "Not Equal"
-} catch (error) {
-  console.error(error.message);
-}
+const counter = createCounter(5);
+
+console.log(counter.increment()); // Output: 6
+console.log(counter.reset()); // Output: 5
+console.log(counter.decrement()); // Output: 4
